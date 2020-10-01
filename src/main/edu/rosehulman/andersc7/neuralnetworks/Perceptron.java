750e82a3463820e818eb6e9ba99dc69b811479c1
Cooper Anderson <cooper@cooperstuff.ninja>
2020-10-01 13:33:11 -0700

    Perceptron

--- src/main/edu/rosehulman/andersc7/neuralnetworks/Perceptron.java
+++ src/main/edu/rosehulman/andersc7/neuralnetworks/Perceptron.java
@@ -1,18 +1,20 @@
 package edu.rosehulman.andersc7.neuralnetworks;
 
 public class Perceptron {
-	
-	private static int[][] trainingData = {		
-		{0,0,0},
-		{0,1,0},
-		{1,0,0},
-		{1,1,1}}; 
-	
+	private static final int[][] trainingData = {
+		{0, 0, 0},
+		{0, 1, 0},
+		{1, 0, 0},
+		{1, 1, 1}
+	};
+
 	private static int trainingSetSize = 0;
 	private static int networkSize = 0;
 	private static double threshold = 0.5; // experiment
 	private static double[] weights;
-	private static double learningRate = 0.1; //experiment 
+	private static double learningRate = 0.1; // experiment
+
+	private static final int episodes = 10;
 	
 	private static void initNetwork() {
 		trainingSetSize = trainingData.length;
@@ -35,32 +37,48 @@ public class Perceptron {
 		}
 	}
 	
-	private static double stepActivationFunction(double input){
+	private static double stepActivationFunction(double input) {
 		if (input >= threshold) return 1;
 		return 0;
 	}
-	
-	private static void trainNetwork(){
-	// TODO: Implement this method. You want to use the following fields:
-		// trainingSetSize
-		// networkSize
-		// weights array
-		// stepActivationFunction
-		// learningRate
-		// trainingData
-		
-		// calculate input to output unit: weighted sums of the inputs, followed by calling activation function
-		// calculate error: desired output - actual output (i.e. activation)
-		// adjust weights
+
+	/**
+	 * TODO: Implement this method. You want to use the following fields:
+	 *  trainingSetSize
+	 *  networkSize
+	 *  weights array
+	 *  stepActivationFunction
+	 *  learningRate
+	 *  trainingData
+	 *
+	 *  calculate input to output unit: weighted sums of the inputs, followed by calling activation function
+	 *  calculate error: desired output - actual output (i.e. activation)
+	 *  adjust weights
+	 */
+	private static void trainNetwork() {
+		// Iterate once for each episode
+		for (int e = 0; e < episodes; e++) {
+			for (int i = 0; i < trainingSetSize; i++) {
+				// Gather output information for specific training example
+				int[] data = trainingData[i];
+				double desired = data[networkSize], input = 0, error;
+				for (int n = 0; n < networkSize; n++) input += weights[n] * data[n];
+				error = desired - stepActivationFunction(input);
+
+				// Adjust weights accordingly
+				if (error == 0) continue;
+				for (int w = 0; w < networkSize; w++) weights[w] += learningRate * error * data[w];
+			}
+		}
 	}
 	
-	private static void printWeights(){
+	private static void printWeights() {
 		for (int i = 0; i < networkSize; i++){
 			System.out.printf("Weight %d is %f.\n", i, weights[i]);
 		}
 	}
 	
-	private static void testNetwork(){
+	private static void testNetwork() {
 		for (int i = 0; i < trainingSetSize; i++){
 			double inputToNeuron = 0;
 			for (int j = 0; j < networkSize; j++){
@@ -74,11 +92,10 @@ public class Perceptron {
 	}
 	
 	
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		initNetwork();
 		trainNetwork();
 		printWeights();
 		testNetwork();
 	}
-	
 }
